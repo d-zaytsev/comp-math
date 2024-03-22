08cbb1f726dbae102c7f15d7abe505b66bdd108c
dmitriy <st107687@student.spbu.ru>
2024-03-22 11:49:55 +0300

    Fix python drawer

--- 1-WaveParallelizer/src/python/drawer.py
+++ 1-WaveParallelizer/src/python/drawer.py
@@ -2,7 +2,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 import sys
 
-file = np.loadtxt("../../build/grid.txt")
+build_folder = sys.argv[1]
+file = np.loadtxt(build_folder + "/grid.txt")
 
 x = np.linspace(0, 1, file.shape[0])
 y = np.linspace(0, 1, file.shape[1])
@@ -16,5 +17,5 @@ ax.set_xlabel('X')
 ax.set_ylabel('Y')
 ax.set_zlabel('Z')
 
-plt.savefig("../../build/picture.png")
+plt.savefig(build_folder + "/picture.png")
 plt.show()
