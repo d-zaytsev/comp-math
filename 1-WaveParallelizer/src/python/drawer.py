5921785b9867a53777b02ca4779185683e04d609
dmitriy <st107687@student.spbu.ru>
2024-03-22 11:46:19 +0300

    python: change path

--- 1-WaveParallelizer/src/python/drawer.py
+++ 1-WaveParallelizer/src/python/drawer.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import sys
 
-file = np.loadtxt("1-WaveParallelizer/build/grid.txt")
+file = np.loadtxt("../../build/grid.txt")
 
 x = np.linspace(0, 1, file.shape[0])
 y = np.linspace(0, 1, file.shape[1])
@@ -16,5 +16,5 @@ ax.set_xlabel('X')
 ax.set_ylabel('Y')
 ax.set_zlabel('Z')
 
-plt.savefig("1-WaveParallelizer/build/picture.png")
+plt.savefig("../../build/picture.png")
 plt.show()
